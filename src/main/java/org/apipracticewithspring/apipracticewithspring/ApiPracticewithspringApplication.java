0369cbc21d8ce4f50457cfcce71eb11b6a993490
satish <sateesh9603@gmail.com>
2024-01-07 12:29:35 +0530

    implemented getsingle,addnewproduct

---
+++ src/main/java/org/apipracticewithspring/apipracticewithspring/ApiPracticewithspringApplication.java
@@ -0,0 +1,13 @@
+package org.apipracticewithspring.apipracticewithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPracticewithspringApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ApiPracticewithspringApplication.class, args);
+    }
+}
---
+++ src/test/java/org/apipracticewithspring/apipracticewithspring/ApiPracticewithspringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.apipracticewithspring.apipracticewithspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPracticewithspringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
